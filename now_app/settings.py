3c8d5375831e17802420d55771a97f4e2f1dc481
EC2 Default User <ec2-user@ip-172-31-28-144.us-east-2.compute.internal>
2019-03-13 14:52:27 +0000

    Adding DB URL to now.json

--- now_app/settings.py
+++ now_app/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 
+import dj_database_url
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -25,7 +27,7 @@ SECRET_KEY = '%n*nfl2s%w!dxtx&hhnzb(%c^5n8wv7k6$fjt&tz!)kvrolmjr'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost','.now.sh']
+ALLOWED_HOSTS = ['*','.now.sh']
 
 
 # Application definition
